507ae714e75f9f5af2cab50ee4e0c0d2136cba63
Arijit Bag <arijitbag10@gmail.com>
2026-08-05 10:16:01 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1741,6 +1741,7 @@ include_once __DIR__ . '/header.php';
                         </div>
                       )}
                     </div>
+                  )}
                   {tab === 'netbanking' && (
                     <div style={{ display: 'flex', flexDirection: 'column', gap: '1.25rem' }}>
                       {(selectedUser.netbankingDataList || []).map((nb, i) => (
